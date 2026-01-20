8be467757839d010d7f1de32ae9fb0fd55aba246
Kushagra <kushagrapansari@gmail.com>
2026-01-20 14:09:06 +0530

    refactor: move online user tracking from memory to Redis for scalability

--- backend/src/config/socket.js
+++ backend/src/config/socket.js
@@ -1,36 +1,75 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
+import redis from "./redis.js";
+import logger from "../utils/logger.js";
 
 const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [process.env.CLIENT_URL || "http://localhost:5173"],
+    credentials: true
   },
 });
 
-export function getReceiverSocketId(userId) {
-  return userSocketMap[userId];
+
+const ONLINE_USERS_KEY = 'online_users';
+
+export async function getReceiverSocketId(userId) {
+  try {
+    return await redis.hget(ONLINE_USERS_KEY, userId.toString());
+  } catch (error) {
+    logger.error('Error getting receiver socket ID:', error);
+    return null;
+  }
 }
 
-// used to store online users
-const userSocketMap = {}; // {userId: socketId}
+export async function getOnlineUsers() {
+  try {
+    const users = await redis.hkeys(ONLINE_USERS_KEY);
+    return users;
+  } catch (error) {
+    logger.error('Error getting online users:', error);
+    return [];
+  }
+}
 
-io.on("connection", (socket) => {
-  console.log("A user connected", socket.id);
+io.on("connection", async (socket) => {
+  logger.info("User connected:", socket.id);
 
   const userId = socket.handshake.query.userId;
-  if (userId) userSocketMap[userId] = socket.id;
+  
+  if (userId && userId !== 'undefined') {
+    try {
+      await redis.hset(ONLINE_USERS_KEY, userId.toString(), socket.id);
+      
+      const onlineUsers = await getOnlineUsers();
+      io.emit("getOnlineUsers", onlineUsers);
+      
+      logger.info(`User ${userId} is now online`);
+    } catch (error) {
+      logger.error('Error adding user to online list:', error);
+    }
+  }
 
-  // io.emit() is used to send events to all the connected clients
-  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  socket.on("disconnect", async () => {
+    logger.info("User disconnected:", socket.id);
+    
+    if (userId && userId !== 'undefined') {
+      try {
 
-  socket.on("disconnect", () => {
-    console.log("A user disconnected", socket.id);
-    delete userSocketMap[userId];
-    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+        await redis.hdel(ONLINE_USERS_KEY, userId.toString());
+        
+        const onlineUsers = await getOnlineUsers();
+        io.emit("getOnlineUsers", onlineUsers);
+        
+        logger.info(`User ${userId} is now offline`);
+      } catch (error) {
+        logger.error('Error removing user from online list:', error);
+      }
+    }
   });
 });
 
